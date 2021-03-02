8c6208b9eee26446796fae558628d63958e75e0c
RussellRuslan <SamatovR@aetna.com>
2021-03-02 01:37:32 -0500

    convert DTO

--- src/main/java/com/cybertek/implemetation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implemetation/RoleServiceImpl.java
@@ -1,15 +1,27 @@
 package com.cybertek.implemetation;
 
 import com.cybertek.dto.RoleDTO;
+import com.cybertek.entity.Role;
+import com.cybertek.repository.RoleRepository;
 import com.cybertek.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
     @Override
     public List<RoleDTO> listAllRoles() {
-        return null;
+       List<Role>list=roleRepository.findAll();
+
+       // convert to DTO and return it (list of roles) Mapper
+       return null;
+
     }
 
     @Override
