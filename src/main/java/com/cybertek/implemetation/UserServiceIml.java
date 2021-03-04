27f72c8c381c16a544314871fe0c884ef7b0c146
RussellRuslan <SamatovR@aetna.com>
2021-03-04 00:35:49 -0500

    html added

--- src/main/java/com/cybertek/implemetation/UserServiceIml.java
+++ src/main/java/com/cybertek/implemetation/UserServiceIml.java
@@ -2,6 +2,7 @@ package com.cybertek.implemetation;
 
 import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.User;
+import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.UserRepository;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,20 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserServiceIml implements UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    UserMapper userMapper;
 
     @Override
     public List<UserDTO> listAllUsers() {
        List<User> listUsers =userRepository.findAll(Sort.by("firstName"));
 
-        return null;
+        return listUsers.stream().map(obj -> {return userMapper.convertToDto(obj);}).collect(Collectors.toList());
     }
 
     @Override
