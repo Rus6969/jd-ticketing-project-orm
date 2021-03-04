d8848f3778caf1fc4cd9ef5b84ce3196c3ca549b
RussellRuslan <SamatovR@aetna.com>
2021-03-03 20:31:08 -0500

    USer DTO convwertion is completed

--- src/main/java/com/cybertek/implemetation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implemetation/RoleServiceImpl.java
@@ -2,34 +2,42 @@ package com.cybertek.implemetation;
 
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.entity.Role;
+import com.cybertek.mapper.RoleMapper;
 import com.cybertek.repository.RoleRepository;
 import com.cybertek.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-// injected this repository to bring data from data base , however we bring it as entity , thats wahy we need to have
+    // injected this repository to bring data from data base , however we bring it as entity , thats wahy we need to have
     // mapper to converrt it to dto and bring to UI
     @Autowired
     private RoleRepository roleRepository;
-
+    @Autowired
+    private RoleMapper roleMapper;
 
     @Override
     // dto
     public List<RoleDTO> listAllRoles() {
         //entity
-       List<Role>list=roleRepository.findAll();
+        List<Role> list = roleRepository.findAll();
 
-       // convert to DTO and return it (list of roles)  mapp to DTO and return to controller
-       return null;
+        // convert to DTO and return it (list of roles)  mapp to DTO and return to controller
+
+        return list.stream().map(obj -> {
+            return roleMapper.convertToDto(obj);
+        }).collect(Collectors.toList());
 
     }
 
     @Override
     public RoleDTO findById(Long id) {
-        return null;
+     Role role= roleRepository.findById(id).get();
+     return  roleMapper.convertToDto(role);
+
     }
 }
