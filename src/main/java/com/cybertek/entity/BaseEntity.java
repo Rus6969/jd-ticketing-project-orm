bec4ef112b18e79012f12814fb28aa45671e6344
RussellRuslan <SamatovR@aetna.com>
2021-03-06 15:13:05 -0500

    nullable annotations are added

--- src/main/java/com/cybertek/entity/BaseEntity.java
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -14,26 +14,33 @@ import java.time.LocalDateTime;
 @Setter
 @MappedSuperclass
 public class BaseEntity {
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //nullable false updatable false we do not want updatre the value it should be the same
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
-    private Boolean isDeleted =false;
-// before saving object in db
+    private Boolean isDeleted = false;
+
+    // before saving object in db
     @PrePersist
-    private void onPrePersist(){
-    this.insertDateTime=LocalDateTime.now();
-    this.lastUpdateDateTime =LocalDateTime.now();
-    this.lastUpdateUserId=1l;
-    this.insertUserId=1L;
+    private void onPrePersist() {
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1l;
+        this.insertUserId = 1L;
     }
+
     @PreUpdate
-    public void onPreUpdaate(){
-        this.lastUpdateDateTime=LocalDateTime.now();
-        this.lastUpdateUserId=1L;
+    public void onPreUpdaate() {
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
     }
 
 
