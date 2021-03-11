35fbdb6f7ffb4e1aa0b0737f3f57a96e1fc6f3a5
RussellRuslan <SamatovR@aetna.com>
2021-03-10 21:59:15 -0500

    task implementation mapper task dto

--- src/main/java/com/cybertek/implemetation/TaskServiceImpl.java
+++ src/main/java/com/cybertek/implemetation/TaskServiceImpl.java
@@ -1,17 +1,14 @@
 package com.cybertek.implemetation;
-
 import com.cybertek.dto.TaskDTO;
-import com.cybertek.entity.Project;
 import com.cybertek.entity.Task;
+import com.cybertek.enums.Status;
 import com.cybertek.mapper.TaskMapper;
-import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.TaskRepository;
 import com.cybertek.repository.UserRepository;
 import com.cybertek.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,10 +33,14 @@ public class TaskServiceImpl implements TaskService {
 
         List<Task> list = taskRepository.findAll();
         return list.stream().map(obj->{return taskMapper.convertToDto(obj);}).collect(Collectors.toList());
+        // lambda expression practice
+        // return list.stream().map(taskMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public Task save(TaskDTO dto) {
+        dto.setTaskStatus(Status.OPEN);
+        dto.setAssignedDate(LocalDate.now());
         Task task= taskMapper.convertToEntity(dto);
       return   taskRepository.save(task);
 
